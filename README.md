1c2cd93ee4634a184d4059ca8257c13a0790e363
Eyaa <czk773@gmail.com>
2025-07-12 17:13:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vite+Vue3+Electron+Typescript template
+
+## Get Started
+
+### 1. clone this repository
+
+```shell
+git clone https://github.com/EyaaCai/vue3-template.git
+```
+
+### 2. install packages
+
+```shell
+npm i
+```
+
+### 3. run
+
+```shell
+npm run dev
+```
---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import vueDevTools from 'vite-plugin-vue-devtools';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+            @use "@/styles/mixins.scss" as *;
+          `,
+      },
+    },
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove();
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
+});
